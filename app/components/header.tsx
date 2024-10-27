56f0f9429579c9ccbf59f2957eb72cd6ffacca3f
Richard Morel <git@rmorel.com>
2024-10-27 19:40:33 +0000

    Visual change

--- app/components/header.tsx
+++ app/components/header.tsx
@@ -2,8 +2,8 @@ import Image from "next/image";
 
 export default function Header() {
   return (
-    <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-      <div className="fixed bottom-0 left-0 mb-4 flex h-auto w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:w-auto lg:bg-none lg:mb-0">
+    <div className="z-10 max-w-5xl w-full flex items-center justify-center font-mono text-sm">
+      <div className="fixed top-0 left-0 right-0 flex h-auto items-center justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:bg-none lg:static lg:w-auto lg:mb-0">
         <a
           href="https://idapt.ai/"
           className="flex items-center justify-center font-nunito text-lg font-bold gap-2"
