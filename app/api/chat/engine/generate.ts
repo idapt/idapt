bc1f32391c9229def120d8fa9f2c97d016a97366
Richard Morel <git@rmorel.com>
2024-10-28 14:29:09 +0000

    Added postgres usage for document and vector store.

--- app/api/chat/engine/generate.ts
+++ app/api/chat/engine/generate.ts
@@ -1,11 +1,14 @@
-import { VectorStoreIndex } from "llamaindex";
-import { storageContextFromDefaults } from "llamaindex/storage/StorageContext";
+import {
+  VectorStoreIndex,
+  PostgresDocumentStore,
+  PostgresIndexStore,
+  storageContextFromDefaults,
+} from "llamaindex";
 
 import * as dotenv from "dotenv";
 
 import { getDocuments } from "./loader";
 import { initSettings } from "./settings";
-import { STORAGE_CACHE_DIR } from "./shared";
 
 // Load environment variables from local .env file
 dotenv.config();
@@ -21,9 +24,20 @@ async function generateDatasource() {
   console.log(`Generating storage context...`);
   // Split documents, create embeddings and store them in the storage context
   const ms = await getRuntime(async () => {
+
     const storageContext = await storageContextFromDefaults({
-      persistDir: STORAGE_CACHE_DIR,
+      docStore: new PostgresDocumentStore(
+        {
+          tableName: "idapt_docs",          
+        }
+      ),
+      indexStore: new PostgresIndexStore(
+        {
+          tableName: "idapt_indexes",
+        }
+      ),
     });
+
     const documents = await getDocuments();
 
     await VectorStoreIndex.fromDocuments(documents, {
--- app/api/chat/engine/index.ts
+++ app/api/chat/engine/index.ts
@@ -1,19 +1,35 @@
-import { SimpleDocumentStore, VectorStoreIndex } from "llamaindex";
-import { storageContextFromDefaults } from "llamaindex/storage/StorageContext";
-import { STORAGE_CACHE_DIR } from "./shared";
+import {
+  VectorStoreIndex,
+  storageContextFromDefaults,
+} from "llamaindex";
+// Need to import from the node_modules folder otherwise we get a webpack error
+import { PostgresDocumentStore } from "llamaindex/storage/docStore/PostgresDocumentStore";
+import { PostgresIndexStore } from "llamaindex/storage/indexStore/PostgresIndexStore";
 
 export async function getDataSource(params?: any) {
-  const storageContext = await storageContextFromDefaults({
-    persistDir: `${STORAGE_CACHE_DIR}`,
-  });
+  try {
+    const storageContext = await storageContextFromDefaults({
+      docStore: new PostgresDocumentStore({
+        tableName: "idapt_docs",
+      }),
+      indexStore: new PostgresIndexStore({
+        tableName: "idapt_indexes",
+      }),
+    });
 
-  const numberOfDocs = Object.keys(
-    (storageContext.docStore as SimpleDocumentStore).toDict(),
-  ).length;
-  if (numberOfDocs === 0) {
-    return null;
+    const numberOfDocs = await (storageContext.docStore as PostgresDocumentStore)
+      .docs()
+      .then((docs) => Object.keys(docs).length);
+
+    if (numberOfDocs === 0) {
+      return null;
+    }
+
+    return await VectorStoreIndex.init({
+      storageContext,
+    });
+  } catch (error) {
+    console.error("Error initializing data source:", error);
+    throw error;
   }
-  return await VectorStoreIndex.init({
-    storageContext,
-  });
 }
