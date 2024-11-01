fb071aadee122c71e380ac0e8ebb444b38e8dacd
Richard Morel <git@rmorel.com>
2024-11-01 16:10:23 +0100

    UI improvements

--- frontend/app/components/header.tsx
+++ frontend/app/components/header.tsx
@@ -2,8 +2,8 @@ import Image from "next/image";
 
 export default function Header() {
   return (
-    <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-      <div className="fixed bottom-0 left-0 mb-4 flex h-auto w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:w-auto lg:bg-none lg:mb-0">
+    <div className="z-10 w-full flex items-center justify-center">
+      <div className="flex items-center justify-center py-2">
         <a
           href="https://www.idapt.ai/"
           className="flex items-center justify-center font-nunito text-lg font-bold gap-2"
--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -4,9 +4,9 @@ import ChatSection from "./components/chat-section";
 export default function Home() {
   return (
     <main className="h-screen w-screen flex justify-center items-center background-gradient">
-      <div className="space-y-2 lg:space-y-10 w-[90%] lg:w-[60rem]">
+      <div className="space-y-0 w-[90%] h-full">
         <Header />
-        <div className="h-[80vh] flex">
+        <div className="flex h-[calc(95vh-64px)] min-h-[400px]">
           <ChatSection />
         </div>
       </div>
