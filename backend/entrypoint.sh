5287d5753833dfbfa79b92e757186f987f65310d
DrachirLerom <github@rmorel.com>
2024-12-22 10:48:40 +0100

    Fix oauth2 client secret permission issues

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -5,7 +5,7 @@ wait_for_client_secret() {
     local secret_file="/keycloak_backend_client_secret/BACKEND_CLIENT_SECRET"
     until [ -f "$secret_file" ] && [ -r "$secret_file" ] && [ -s "$secret_file" ]; do
         echo "Waiting for Keycloak client secret..."
-        sleep 5
+        sleep 1
     done
     echo "Client secret is available"
 }
