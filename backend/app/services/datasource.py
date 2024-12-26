1f216efbceaf6d16ac78c07a7d2b4c4c7622733f
DrachirLerom <github@rmorel.com>
2024-12-26 16:12:49 +0100

    Improve datasource vector store creation.

--- backend/app/services/datasource.py
+++ backend/app/services/datasource.py
@@ -8,7 +8,6 @@ from llama_index.vector_stores.postgres import PGVectorStore
 from llama_index.storage.docstore.postgres import PostgresDocumentStore
 from llama_index.storage.index_store.postgres import PostgresIndexStore
 from llama_index.core.indices import VectorStoreIndex
-from llama_index.core.storage.storage_context import StorageContext
 from llama_index.core.tools import BaseTool
 from app.engine.tools.filtered_query_engine import FilteredQueryEngineTool
 from llama_index.core.tools import ToolMetadata
@@ -192,7 +191,7 @@ class DatasourceService:
                 port=url.port,
                 user=url.username,
                 schema_name="public",
-                table_name=f"embeddings_{datasource_identifier}",
+                table_name=f"{datasource_identifier}_embeddings",
                 embed_dim=int(app_settings.embedding_dim),
                 perform_setup=True,
                 #hnsw_kwargs={
@@ -212,7 +211,7 @@ class DatasourceService:
             return PostgresDocumentStore.from_uri(
                 uri=get_connection_string(),
                 schema_name="public",
-                table_name=f"docstore_{datasource_identifier.lower()}"
+                table_name=f"{datasource_identifier}_docstore"
             )
         except Exception as e:
             self.logger.error(f"Error creating doc store: {str(e)}")
@@ -224,7 +223,7 @@ class DatasourceService:
             return PostgresIndexStore.from_uri(
                 uri=get_connection_string(),
                 schema_name="public",
-                table_name=f"index_{datasource_identifier.lower()}"
+                table_name=f"{datasource_identifier}_index"
             )
         except Exception as e:
             self.logger.error(f"Error creating index store: {str(e)}")
@@ -232,16 +231,23 @@ class DatasourceService:
 
     def _create_index(self, datasource_identifier: str) -> VectorStoreIndex:
         try:
-            vector_store, doc_store, index_store = self.get_storage_components(datasource_identifier)
-            storage_context = StorageContext.from_defaults(
+            vector_store, doc_store, _ = self.get_storage_components(datasource_identifier)
+            # Recreate the index from the vector store and doc store at each app restart if needed
+            index = VectorStoreIndex.from_vector_store(
                 vector_store=vector_store,
                 docstore=doc_store,
-                index_store=index_store
+                #index_store=index_store # Remove index store support for now as it is not thought to be important
             )
-            return VectorStoreIndex(
-                [],  # Empty nodes list for initial creation
-                storage_context=storage_context
-            )
+            return index
+            #storage_context = StorageContext.from_defaults(
+            #    vector_store=vector_store,
+            #    docstore=doc_store,
+            #    index_store=index_store
+            #)
+            #return VectorStoreIndex(
+            #    [],  # Empty nodes list for initial creation
+            #    storage_context=storage_context
+            #)
         except Exception as e:
             self.logger.error(f"Error creating index: {str(e)}")
             raise
