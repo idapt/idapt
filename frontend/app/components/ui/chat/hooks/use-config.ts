5d45086e4e351b2dfc569e08e0d530dae9460bb7
DrachirLerom <git@rmorel.com>
2024-12-31 13:39:47 +0100

    Fix back getBackendOrigin to take into account caller environement

--- frontend/app/components/ui/chat/hooks/use-config.ts
+++ frontend/app/components/ui/chat/hooks/use-config.ts
@@ -4,9 +4,17 @@ export interface ChatConfig {
   backend?: string;
 }
 
+// This function will return the right backend origin depending on where it is called from.
 function getBackendOrigin(): string { 
-    const hostDomain = process.env.NEXT_PUBLIC_HOST_DOMAIN;
-    return `https://${hostDomain}`;  
+    // If this code is executed on the client side / machine.
+    if ( typeof window !== 'undefined') {
+      // Use the current frontend origin as the backend is hosted at the same address, only with /api after as specified in the nginx.conf file.
+      return window.location.origin;
+    }
+    // If this code is executed on the server side, go through the internal docker network to access the backend.
+    else {
+      return `http://idapt-backend:3030`;
+    }
 }
 
 export function useClientConfig(): ChatConfig {
