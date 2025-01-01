2dcaa3934ddf343f213b5637d281fca20b2d5509
DrachirLerom <github@rmorel.com>
2025-01-01 17:13:59 +0100

    Remove unused colbert rerank

--- backend/app/api/routers/chat.py
+++ backend/app/api/routers/chat.py
@@ -2,7 +2,6 @@ import logging
 
 from fastapi import APIRouter, BackgroundTasks, HTTPException, Request, status
 from llama_index.core.llms import MessageRole
-from llama_index.postprocessor.colbert_rerank import ColbertRerank
 
 from app.api.routers.events import EventCallbackHandler
 from app.api.models.models import (
@@ -45,23 +44,6 @@ async def chat(
             event_handlers=[event_handler]
         )
 
-        ## Do a naive first query on the files index with the last message to get initial results and increase responsiveness
-        #files_index = StorageContextSingleton().index
-        #top_k = 4
-        #colbert_reranker = ColbertRerank(
-        #    top_n=top_k,
-        #    model="colbert-ir/colbertv2.0",
-        #    tokenizer="colbert-ir/colbertv2.0",
-        #    keep_retrieval_score=True,
-        #)
-        #files_query_engine = files_index.as_query_engine( 
-        #    **({"similarity_top_k": top_k*2} if top_k != 0 else {}),
-        #    node_postprocessors=[colbert_reranker]
-        #)
-        #initial_query_response = files_query_engine.query(last_message_content)
-        ## Add the results to the last message content at the end of it so that the agent can use it right away
-        #last_message_content = f"{last_message_content}\n\nHere is some context to help you answer the user query:\n##{initial_query_response}"
-  
         # Send the streaming query to the agent
         response = chat_engine.astream_chat(last_message_content, messages)
 
