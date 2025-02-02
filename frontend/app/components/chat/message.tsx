a710e0ac5d13e6177f40ffb663ea108742991c88
DrachirLerom <git@rmorel.com>
2025-02-02 12:01:59 +0100

    Make chat user messages blue.

--- frontend/app/components/chat/message.tsx
+++ frontend/app/components/chat/message.tsx
@@ -101,7 +101,7 @@ const PurePreviewMessage = ({
 
                 <div
                   className={cn('flex flex-col gap-4', {
-                    'bg-primary text-primary-foreground px-3 py-2 rounded-xl':
+                    'bg-[#3a81ff] text-white px-3 py-2 rounded-xl':
                       message.role === 'user',
                   })}
                 >
